fb281027e94986c894ff3d4bfb18a4cda0a5a8f4
R.Florin <florin1994x@mail.ru>
2016-07-01 23:52:53 +0300

    prototype

---
+++ BeeMedia/main-layout/assets/css/images/next_files/deckNP_js.php
@@ -0,0 +1,17 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<a href="http://www.squarespace.com/?channel=display&subchannel=thedeck&source=copy1&campaign=january2016&subcampaign=nonprogrammatic">';
+advert+='<img class="ad" alt="Squarespace" src="//connect.decknetwork.net/i/squares_1601_3r.jpg" border="0" height="90" width="120" />';
+advert+='</a>';
+advert+='<p class="ads">';
+advert+='Create a landing page for your profile, project, or small business. <a href="http://www.squarespace.com/?channel=display&subchannel=thedeck&source=copy1&campaign=january2016&subcampaign=nonprogrammatic">Start your free trial</a>.';
+advert+='</p>';
+
+
+
+
+document.write(advert);
+
+
